4cd4f860bbd42553790118c8f8130a56ca4b4d10
Pavel <135458876+kostuchik@users.noreply.github.com>
2023-10-11 16:58:27 +0300

    [Courier] fear: create sevice, repository and controller

---
+++ src/main/java/com/factglobal/delivery/controllers/CourierController.java
@@ -0,0 +1,46 @@
+package com.factglobal.delivery.controllers;
+
+import com.factglobal.delivery.models.Courier;
+import com.factglobal.delivery.models.Customer;
+import com.factglobal.delivery.services.CourierService;
+import com.factglobal.delivery.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/couriers")
+public class CourierController {
+    private final CourierService courierService;
+
+    @Autowired
+    public CourierController(CourierService courierService) {
+        this.courierService = courierService;
+    }
+
+    @GetMapping("/{id}")
+    public Courier getCourier(@PathVariable("id") int id) {
+        return courierService.getCourier(id);
+    }
+
+    @PostMapping()
+    public ResponseEntity<HttpStatus> addCourier(@RequestBody Courier courier) {
+        courierService.saveCourier(courier);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<HttpStatus> edit(@RequestBody Courier courier) {
+        courierService.saveCourier(courier);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCourier(@PathVariable("id") int id) {
+        courierService.deleteCourier(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/factglobal/delivery/services/CourierService.java
@@ -0,0 +1,37 @@
+package com.factglobal.delivery.services;
+
+
+import com.factglobal.delivery.models.Courier;
+import com.factglobal.delivery.models.Customer;
+import com.factglobal.delivery.repositories.CourierRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CourierService {
+    private final CourierRepository courierRepository;
+
+    @Autowired
+    public CourierService(CourierRepository courierRepository) {
+        this.courierRepository = courierRepository;
+    }
+
+    public Courier getCourier(int id) {
+        Optional<Courier> courier = courierRepository.findById(id);
+        return courier.orElse(null);
+    }
+
+    public void saveCourier(Courier courier) {
+        courierRepository.save(courier);
+    }
+
+    public void deleteCourier(int id) {
+        courierRepository.deleteById(id);
+    }
+
+}
