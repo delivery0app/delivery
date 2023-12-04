abec7fbeec59070eba674ff0b9e5896737f1fb4a
Pavel <135458876+kostuchik@users.noreply.github.com>
2023-12-04 12:19:37 +0300

    [General] ref: fixed an error with too many database connection

--- src/test/java/com/factglobal/delivery/repositories/UserRepositoryTest.java
+++ src/test/java/com/factglobal/delivery/repositories/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.factglobal.delivery.repositories;
 
 import com.factglobal.delivery.models.User;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
